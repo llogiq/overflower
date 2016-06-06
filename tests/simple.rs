a69f9be5a536426811cc9b6dde9858f07dc1503c
Andre Bogus <bogusandre@gmail.com>
2016-06-07 00:55:22 +0200

    Cargo.toml updated, first tests

---
+++ tests/simple.rs
@@ -0,0 +1,33 @@
+#![feature(plugin)]
+#![plugin(overflower)]
+#![allow(const_err)]
+
+extern crate overflower_support;
+use overflower_support::*;
+
+#[test]
+#[overflow(wrap)]
+fn test_simple_wrap() {
+    255u8 + 1;
+}
+
+#[test]
+#[overflow(panic)]
+#[should_panic]
+fn test_simple_panic_sub() {
+    1u8 - 2;
+}
+
+#[test]
+#[overflow(panic)]
+#[should_panic]
+fn test_simple_panic_add() {
+    255u8 + 1;
+}
+
+#[test]
+#[overflow(panic)]
+#[should_panic]
+fn test_macro_panic_add() {
+    assert_eq!(0, 255u8 + 1);
+}
